63c2e554b7a1143e28d02c4a8d8e6d78cd9609a1
SkyNetCloud <mcboxer1201@gmail.com>
2025-02-19 19:42:39 -0500

    Added Permission Node and Update Modded Site info

--- src/main/java/ca/skynetcloud/cobblescheduler/commands/SchedulerCommands.java
+++ src/main/java/ca/skynetcloud/cobblescheduler/commands/SchedulerCommands.java
@@ -7,6 +7,7 @@ import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 
 import com.mojang.brigadier.context.CommandContext;
+import me.lucko.fabric.api.permissions.v0.Permissions;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
@@ -21,7 +22,7 @@ public class SchedulerCommands {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
         dispatcher.register(
                 Commands.literal("CobbleScheduler")
-                        .requires(source -> source.hasPermission(2))
+                        .requires(source -> source.hasPermission(2)).requires(Permissions.require("cobblescheduler.reload"))
                         .then(Commands.literal("reload")
                                 .executes(SchedulerCommands::reloadConfig))
         );
